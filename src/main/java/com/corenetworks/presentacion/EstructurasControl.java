85c2555993ed6030d964c0a171a5e504d3662b85
FaleteRusso <faleterusso89@gmail.com>
2024-01-12 17:51:30 +0100

    17:51

--- src/main/java/com/corenetworks/presentacion/EstructurasControl.java
+++ src/main/java/com/corenetworks/presentacion/EstructurasControl.java
@@ -1,2 +1,100 @@
-package com.corenetworks.presentacion;public class EstructurasControl {
+package com.corenetworks.presentacion;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class EstructurasControl {
+    public static void main(String[] args) {
+        //las estructuras de control son condicionales(if, else y swich)y bucles(for,while,for each,do while)
+        //if -- si la cantidad es mayor a 100 el descuento es 3%, si es menor a 100 1%
+        int cantidad = 80;
+        if (cantidad > 100) {
+            System.out.println("tiene descuento del 3%");
+        } else {
+            System.out.println("Tiene un descuentos del 1%");
+        }
+        //Declara fecha de pedido del 3 de enero de 2024
+        LocalDate fPedido = LocalDate.of(2024, 1, 3);
+        //mostrar antes si la fecha es antes del dia de hoy
+        //mostrar despues si la fecha es despues del dia de hoy
+        if (fPedido.isBefore(LocalDate.now())) {
+            System.out.println("antes");
+        } else {
+            System.out.println("posterior");
+        }
+
+        //La letras E es Ejecucuion, la W esperando, laF terminando en caso de que no sean esas letras, invalido
+        String eProces = "F";
+        switch (eProces.toLowerCase()) {
+            case "e":
+                System.out.println("Ejecución");
+                break;
+            case "f":
+                System.out.println("Terminado");
+                break;
+            case "w":
+                System.out.println("Esperando");
+                break;
+            default:
+                System.out.println("Valor invalido");
+                break;
+        }
+//mostrar por consola descendentemente del 10 al 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        //sumar kos numeros introducidos por consola hasta que el usuario introduzca un 0
+       /* int suma = 0;
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        // Ciclo hasta que se introduce un 0
+        do {
+            System.out.print("Introduce un número (marca 0 para salir): ");
+            num = sc.nextInt();
+
+            // Sumar el número a la suma total
+            suma += num;
+
+        } while (num != 0);
+
+        // Mostrar la suma total
+        System.out.println("La suma total es: " + suma);
+
+
+int suma =0;
+int num =0;
+Scanner sc = new Scanner(System.in);
+while (true){
+    System.out.println("ingrese un numero (0 para salir)");
+    num = sc.nextInt();
+            suma+=num;
+    if(num==0){
+        break;
+    }
 }
+        System.out.println("La suma total es: " + suma);
+
+        */
+
+int[]numeros = {3,2,5,8,10};
+//decir si un numero existe ene el Array y recibirlo por consola
+        //hacerlo con un foreach
+        Scanner sC = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        int numeroB = sC.nextInt();
+
+        // Utilizando un bucle foreach para buscar el número
+        boolean encontrado = false;
+        for (int elemento : numeros) {
+            if (elemento == numeroB) {
+                System.out.println("Numero encontrado");
+                break;
+            }
+        }
+    }
+    }
+
+
--- src/main/java/com/corenetworks/presentacion/Repaso1.java
+++ src/main/java/com/corenetworks/presentacion/Repaso1.java
@@ -1,2 +1,88 @@
-package com.corenetworks.presentacion;public class Repaso1 {
+package com.corenetworks.presentacion;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Repaso1 {
+    public static void main(String[] args) {
+        // Declaracion de variables
+        byte vBy=127;
+        short vS=12345;
+        int vI=1234567;
+        long vL=123456789;
+        Float vF=123.45F;
+        Double vD=12.43;
+        char vC='a';
+        boolean vBo=true;
+        //Clases envolventes
+        Byte eByte=17;
+        Short eShort=12345;
+        Integer eInt=1234567;
+        Long eLong= 12345644L;
+        Float eFloat =34.56F;
+        Double eDouble = 23344.76;
+        Character eChar= 'd';
+        Boolean eBoolean=true;
+        //convertir un carácter del valor 5 a integer
+        int variable = Integer.parseInt("5");
+        //Convertit un String con valor 5.7 a Double
+        double variableString= Double.parseDouble("5.7");
+        System.out.println(variableString);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribir un numero con decimales -> ");
+        //double nPorConsola = sc.nextDouble();
+
+        //Convertir un Double a String
+        System.out.println("Double convertido a String ->"+eDouble.toString());
+        Object o = new Object();
+        System.out.println("MetodotoString de o ->"+o.toString());
+
+        //String
+        String nombre = "Juan";
+        String apellido = "López";
+        System.out.println("Nombre completo ->"+nombre + " "+apellido);
+        //souf es mas rapido, %s en vez de concadenar con +
+        System.out.printf("Nombre completo %s %s",nombre,apellido);
+        //Definir edad y sueldo (entero y double)
+        int edad = 18;
+        double sueldo = 1500.546;
+       System.out.printf("Nombre completo %s %s, tiene %d y gana %.2f %n",nombre,apellido,edad,sueldo);
+
+        System.out.println(nombre.toUpperCase());
+        System.out.println(nombre.toLowerCase());
+        System.out.println(nombre.indexOf("a"));
+        System.out.println(nombre.replace("a","A"));
+        System.out.println(nombre.contains("n"));
+        System.out.println(nombre.substring(1,3));
+
+        String valores= "1/5/8/10";
+        String[]valoresSeparados= valores.split("/");
+        System.out.println(Arrays.toString(valoresSeparados));
+
+        //Solicitar una frase y dividirlo por palabras
+        String frase=null;
+        System.out.println("Escriba una frase ->");
+        /*frase = sc.nextLine();
+        String []fraseSeparada = frase.split(" ");
+        System.out.println(Arrays.toString(fraseSeparada));
+
+         */
+
+        //Fechas
+        LocalDate fCumple = LocalDate.of(1989,6,24);
+        System.out.println(fCumple);
+
+        //Obtener edad
+        System.out.println(Period.between(fCumple,LocalDate.now()).getYears());
+        System.out.println(LocalDate.now().plusDays(30));
+        System.out.println(LocalDate.now().minusMonths(1).minusDays(6));
+
+
+
+    }
 }
+
